ff17fa183ce9f1c48fe0a909cfeea531f6fa6f3a
wilkersoh <plgu@hotmail.com>
2022-08-24 10:52:06 +0800

    Add Prefetch cache state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Login from "./features/auth/Login";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
+import Prefetch from "./features/auth/Prefetch";
 
 function App() {
 	return (
@@ -14,16 +19,29 @@ function App() {
 				<Route index element={<Public />} />
 				<Route path="login" element={<Login />} />
 
-				{/* protected route */}
-				<Route path="dash" element={<DashLayout />}>
-					<Route index element={<Welcome />} />
+				{/*
+						1. Remain the state after refresh
+							eg: if not prefetch: /dash/users/:id -> refresh -> page showing loading
+							- We don't call api on /dash/users/:id, we use selector to get the object in store
+						2. prevent keepUnusedDataFor refresh after default second
 
-					<Route path="notes">
-						<Route index element={<NotesList />} />
-					</Route>
+				*/}
+				<Route element={<Prefetch />}>
+					{/* protected route */}
+					<Route path="dash" element={<DashLayout />}>
+						<Route index element={<Welcome />} />
 
-					<Route path="users">
-						<Route index element={<UsersList />} />
+						<Route path="users">
+							<Route index element={<UsersList />} />
+							<Route path=":id" element={<EditUser />} />
+							<Route path="new" element={<NewUserForm />} />
+						</Route>
+
+						<Route path="notes">
+							<Route index element={<NotesList />} />
+							<Route path=":id" element={<EditNote />} />
+							<Route path="new" element={<NewNote />} />
+						</Route>
 					</Route>
 				</Route>
 				{/* end protected route */}
--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -15,7 +15,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 			validateStatus: (response, result) => {
 				return response.status === 200 && !result.isError;
 			},
-			keepUnusedDataFor: 5, // default is 60s
+			// keepUnusedDataFor: 5, // default is 60s, after the second it back to loading screen
 			transformResponse: (responseData) => {
 				// transform the data from backend
 				const loadedUsers = responseData.map((user) => {
@@ -25,6 +25,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 				return usersAdapter.setAll(initialState, loadedUsers);
 			},
 			providesTags: (result, error, arg) => {
+				// the Tag must be register the tagTypes in the store
 				if (result?.ids) {
 					return [
 						{ type: "User", id: "LIST" },
@@ -33,10 +34,48 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 				} else return [{ type: "User", id: "LIST" }];
 			},
 		}),
+		addNewUser: builder.mutation({
+			query: (initialUserData) => ({
+				url: "/users",
+				method: "POST",
+				body: {
+					...initialUserData,
+				},
+			}),
+			// Focus Cache to update the User's List
+			invalidatesTags: [{ type: "User", id: "LIST" }],
+		}),
+		updateUser: builder.mutation({
+			query: (initialUserData) => ({
+				url: "/users",
+				method: "PATCH",
+				body: {
+					...initialUserData,
+				},
+			}),
+			/*
+				Focus Cache to update the User's id ( only one )
+				it works because we provided the id tag in getUsers's providesTags fn
+			*/
+			invalidatesTags: (result, error, arg) => [{ type: "User", id: arg.id }],
+		}),
+		deleteUser: builder.mutation({
+			query: ({ id }) => ({
+				url: `/users`,
+				method: "DELETE",
+				body: { id },
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: "User", id: arg.id }],
+		}),
 	}),
 });
 
-export const { useGetUsersQuery } = usersApiSlice;
+export const {
+	useGetUsersQuery,
+	useAddNewUserMutation,
+	useUpdateUserMutation,
+	useDeleteUserMutation,
+} = usersApiSlice;
 
 // returns the query result object
 export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
